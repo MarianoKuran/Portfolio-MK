17144d44c485a089fb1e59a46ae35dc7c02964c0
Mariano Kuran <marianokuran6@gmail.com>
2022-10-10 12:09:46 -0300

    add project deploy and link to github repository

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Works from "../images/Works.png";
+import wikidogs from "../images/wikidogs.png";
 import {BiCodeAlt} from "react-icons/bi"
 import {GiBoltEye} from "react-icons/gi"
 
@@ -17,7 +18,7 @@ const Projects = () => {
       <div className='flex items-center  w-[70%] h-[250px] p-4 rounded-sm shadow-md'>
         {/* IMAGE */}
         <div className='flex items-center h-full w-[30%] shadow-md'>
-          <img src="" alt="Work picture" />
+          <img src={wikidogs} alt="Work picture" />
         </div>
         {/* INFO  */}
         <div className='flex flex-col items-center h-full w-[50%] mx-14 border-r-2 border-[#eee]'>
@@ -26,12 +27,12 @@ const Projects = () => {
         </div>
         {/* BUTTONS */}
         <div className='flex flex-col justify-around h-full w-[10%]'>
-          <a href="https://github.com/MarianoKuran" target="_blank">
+          <a href="https://github.com/MarianoKuran/PI-Dogs-main" target="_blank">
             <div id='icon-code-1' className='flex items-center justify-center p-1 h-[50px] w-[50px] rounded-[50%] bg-[#f6f6f6] shadow-md'>
                 <BiCodeAlt size={35} fill={"#333"}/> 
             </div>
           </a>
-          <a href="https://github.com/MarianoKuran" target="_blank">
+          <a href="https://wikidogs-indol.vercel.app/" target="_blank">
             <div id='icon-code-1' className='flex items-center justify-center p-1 h-[50px] w-[50px] rounded-[50%] bg-[#f6f6f6] shadow-md'>
                 <GiBoltEye size={35} fill={"#333"}/> 
             </div>
