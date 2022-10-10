d312d83f748cfcaf12f5333d7a0602c24975edfe
Mariano Kuran <marianokuran6@gmail.com>
2022-10-10 12:50:22 -0300

    add new styles in project card

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -15,18 +15,28 @@ const Projects = () => {
       </div>
 
       {/* CARD */}
-      <div className='flex items-center  w-[70%] h-[250px] p-4 rounded-sm shadow-md'>
+      <div className='flex items-center  w-[70%] h-fit p-4 rounded-sm shadow-md'>
         {/* IMAGE */}
-        <div className='flex items-center h-full w-[30%] shadow-md'>
+        <div className='flex items-center h-fit w-[30%] shadow-md'>
           <img src={wikidogs} alt="Work picture" />
         </div>
         {/* INFO  */}
-        <div className='flex flex-col items-center h-full w-[50%] mx-14 border-r-2 border-[#eee]'>
-          <h3 className='mt-2 font-semibold border-b-2 border-[#ddd]'>PORYECTO 1</h3>
-          <p className='mt-5 text-[#222] italic'>descripcion del proyecto</p>
+        <div className='flex flex-col items h-full w-[60%] p-4 mx-5 border-r-2 border-[#eee]'>
+          <h3 className='mt-2 font-semibold text-[#333] border-b-2 border-[#ddd]'>WIKIDOGS</h3>
+          <p className='mt-2 text-[#333] italic text-justify'>Es una aplicación en la cual se pueden ver distintas razas de perro junto con información relevante de las mismas utilizando la api externa the dog api y a partir de ella poder, entre otras cosas: Buscar perros / Filtrarlos / Ordenarlos / Agregar nuevos perros</p>
+          <div className='flex flex-col items-start h-fit mt-2'>
+            <div><p className='w-fit mb-2 text-[#333] font-semibold italic border-b-[1px] border-[#333]'>Tecnologias utilizadas</p></div>
+            <div>
+              <span className='h-fit mx-1 pr-2 border-r-[1px] border-[#ddd] font-bold text-[#444]'>React</span> 
+              <span className='h-fit mx-1 pr-2 border-r-[1px] border-[#ddd] font-bold text-[#444]'>CSS</span> 
+              <span className='h-fit mx-1 pr-2 border-r-[1px] border-[#ddd] font-bold text-[#444]'>Express</span> 
+              <span className='h-fit mx-1 pr-2 border-r-[1px] border-[#ddd] font-bold text-[#444]'>PostgreSQL</span> 
+              <span className='h-fit mx-1 pr-2 border-r-[1px] border-[#ddd] font-bold text-[#444]'>Sequelize</span>
+            </div>
+          </div>
         </div>
         {/* BUTTONS */}
-        <div className='flex flex-col justify-around h-full w-[10%]'>
+        <div className='flex flex-col items-center justify-around h-full w-[5%]'>
           <a href="https://github.com/MarianoKuran/PI-Dogs-main" target="_blank">
             <div id='icon-code-1' className='flex items-center justify-center p-1 h-[50px] w-[50px] rounded-[50%] bg-[#f6f6f6] shadow-md'>
                 <BiCodeAlt size={35} fill={"#333"}/> 
