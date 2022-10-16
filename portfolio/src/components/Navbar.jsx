78d76734c175818f6495143ddb08b973c11adc3a
Mariano Kuran <marianokuran6@gmail.com>
2022-10-16 06:17:19 -0300

    fix navigation whit navbar icons

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React, { useState }  from 'react'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
-import {MdWork} from 'react-icons/md'
+import {MdWork, MdContactMail} from 'react-icons/md'
 import {GiTechnoHeart} from 'react-icons/gi'
-import {GrContact} from 'react-icons/gr'
 
 const Navbar = () => {
 
   function clickHandler(e) {
     e.preventDefault();
     const href = e.target.getAttribute("href");
+    const section = document.querySelector(href)
+    const offsetTop = section.offsetTop;
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    }); 
+  }
+  function iconScroll(e) {
+    e.preventDefault();
+    let href;
+    e.target.id ? href =  e.target.id : "cuak"
+    e.target.parentNode.id ? href = e.target.parentNode.id : "cuak"
+    e.target.parentNode.parentNode.id ? href = e.target.parentNode.parentNode.id : "cuak"
+
     const section = document.querySelector(href)
     const offsetTop = section.offsetTop;
     scroll({
@@ -38,19 +51,19 @@ const Navbar = () => {
           </a>
         </div>
         {/* nav Mobile */}
-        <div className='absolute bottom-0 md:hidden flex items-center justify-around h-full w-full bg-[#f6f6f6]'>
-          <a onClick={ e => clickHandler(e)} href="#About_me" className='flex items-center justify-center h-full w-[25%]  bg-[#f6f6f6]'>
-            <BsFillPersonLinesFill size={32} fill="#333" />
-          </a>
-          <a onClick={ e => clickHandler(e)} href="#Works" className='flex items-center justify-center h-full w-[25%]  bg-[#f6f6f6]'>
-            <MdWork size={32}  fill="#333"/>
-          </a>
-          <a onClick={ e => clickHandler(e)} href="#Skills" className='flex items-center justify-center h-full w-[25%]  bg-[#f6f6f6]'>
-            <GiTechnoHeart size={32}  fill="#333"/>
-          </a>
-          <a onClick={ e => clickHandler(e)} href="#Contact" className='flex items-center justify-center h-full w-[25%]  bg-[#f6f6f6]'>
-            <GrContact size={32} fill="#333" />
-          </a>
+        <div className='absolute bottom-0 md:hidden flex items-center justify-around h-full w-full border-t-[1px] shadow-2xl'>
+          <div id="#About_me"  onClick={ e => iconScroll(e)} className='relative z-50 flex items-center justify-center h-full w-[25%]'>
+            <BsFillPersonLinesFill size={40} fill="#222" className="h-full" />
+          </div>
+          <div id="#Works" onClick={ e => iconScroll(e)} className='relative z-50 flex items-center justify-center h-full w-[25%]'>
+            <MdWork size={40}  fill="#222" className="h-full" />
+          </div>
+          <div id="#Skills" onClick={ e => iconScroll(e)} className='relative z-50 flex items-center justify-center h-full w-[25%]'>
+            <GiTechnoHeart size={40}  fill="#222" className="h-full" />
+          </div>
+          <div id="#Contact" onClick={ e => iconScroll(e)} className='relative z-50 flex items-center justify-center h-full w-[25%]'>
+            <MdContactMail size={40} fill="#222" className="h-full" />
+          </div>
         </div>
       </nav>
     </header>
