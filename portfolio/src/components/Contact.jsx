cf2704c7daa898e5a3f46a9e477f1ced1d5d4371
Mariano Kuran <marianokuran6@gmail.com>
2022-10-16 08:10:12 -0300

    add responsive design in contact component

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -4,15 +4,16 @@ import { SiGmail, SiWhatsapp, SiInstagram, SiLinkedin } from "react-icons/si"
 
 const Contact = () => {
   return (
-    <section id="Contact" className="flex flex-col items-center w-full h-fit">
-      <div className='flex flex-col items-center w-[70%] h-full '>
-        <div className="flex items-start w-[70%] h-[100px] my-7">
-          <img src={Contact_png} alt="Skills" className="relative z-0 h-full w-[25%] drop-shadow-xl mx-auto" />        
+    <section id="Contact" className="flex flex-col items-center w-full h-[700px]">
+      <div className='flex flex-col items-center md:w-[70%] h-full '>
+        <div className="flex items-start w-full md:w-[70%] md:h-[100px] my-7">
+          <p className="md:hidden pl-6 pr-3 text-2xl font-bolder border-r-[1px] border-[#333]">CONTACT</p>
+          <img src={Contact_png} alt="Skills" className="hidden md:block relative z-0 h-full w-[25%] drop-shadow-xl mx-auto" />        
         </div>
         <div className='flex flex-col items-center h-full w-full'>
           <h4>¡Contactame y hagamos algo grande!</h4>
 
-          <form action="https://formsubmit.co/e848edbccb8aa7e9c2efdc3160874b26" method="POST" className='flex flex-col w-[50%] h-[280px] mt-4'>
+          <form action="https://formsubmit.co/e848edbccb8aa7e9c2efdc3160874b26" method="POST" className='flex flex-col w-full md:w-[50%] h-[280px] mt-8 md:mt-4'>
             <div className='relative h-[60px] p-2 mb-4'>
               <input type="email" name='email_transmitter' placeholder='Email' className='peer h-full w-full py-1 px-2 border-b-[1px] border-[#ddd] bg-transparent focus:outline-none focus:border-[#333] transition-colors '/>
               <label for="email_transmitter" className='absolute opacity-0 left-3 top-1 cursor-text transition-all peer-focus:text-xs peer-focus:-top-[4px] peer-focus:opacity-100'>Email</label>
@@ -26,10 +27,10 @@ const Contact = () => {
           <hr className="w-[30%] h-[2px] mb-5 bg-[#333]" />
           <p className='mb-3'>Tambien puedes contactarme por alguna de mis redes</p>
           <div className='flex items-center justify-center w-fit h-[80px] gap-10'>
-            {/* REDES */}
+            {/* socia media */}
 
             {/* SiGmail */}
-            <div className='flex flex-col items-center w-[50px]'>
+            <div className='hidden md:flex flex-col items-center w-[50px]'>
               <SiGmail size={35} fill="#333" className='absolute' />
               <a
                 href="mailto:marianokuran6@gmail.com?Subject=Quiero%20contactarte%20para..."
@@ -40,7 +41,7 @@ const Contact = () => {
               </a>
             </div>
             {/* lINKEDIN */}
-            <div className='flex flex-col items-center w-[50px]'>
+            <div className='hidden md:flex flex-col items-center w-[50px]'>
               <SiLinkedin size={35} fill="#333" className='absolute' />
               <a
                 href="https://www.linkedin.com/in/mariano-kuran-809642236/"
@@ -51,7 +52,7 @@ const Contact = () => {
               </a>
             </div>
             {/* SiInstagram */}
-            <div className='flex flex-col items-center w-[50px]'>
+            <div className='hidden md:flex flex-col items-center w-[50px]'>
               <SiInstagram size={35} fill="#333" className='absolute' />
               <a
                 href="https://www.instagram.com/_kvrxn___/"
@@ -62,12 +63,54 @@ const Contact = () => {
               </a>
             </div>
             {/* SiWhatsapp */}
-            <div className='flex flex-col w-[50px] items-center '>
+            <div className='hidden md:flex flex-col w-[50px] items-center '>
               <SiWhatsapp size={35} fill="#333" className='absolute' />
               <a
                 href="https://bit.ly/3TpnpKp"
                 target="_blank"
                 className="relative opacity-0 flex items-end top-0 h-[65px] duration-100 hover:opacity-100"
+                >
+                Whatsapp
+              </a>
+            </div>
+
+            {/* social media mobile */}
+            <div className='flex flex-col items-center w-[50px]'>
+              <SiGmail size={30} fill="#333" className='' />
+              <a
+                href="mailto:marianokuran6@gmail.com?Subject=Quiero%20contactarte%20para..."
+                target="_blank"
+                className="text-md mt-1"
+                >
+                Gmail
+              </a>
+            </div>
+            <div className='flex flex-col items-center w-[50px]'>
+              <SiLinkedin size={30} fill="#333" className='' />
+              <a
+                href="https://www.linkedin.com/in/mariano-kuran-809642236/"
+                target="_blank"
+                className="text-md mt-1"
+                >
+                LinkedIn
+              </a>
+            </div>
+            <div className='flex flex-col items-center w-[50px]'>
+              <SiInstagram size={30} fill="#333" className='' />
+              <a
+                href="https://www.instagram.com/_kvrxn___/"
+                target="_blank"
+                className="text-md mt-1"
+                >
+                Instagram
+              </a>
+            </div>
+            <div className='flex flex-col w-[50px] items-center '>
+              <SiWhatsapp size={30} fill="#333" className='' />
+              <a
+                href="https://bit.ly/3TpnpKp"
+                target="_blank"
+                className="text-md mt-1"
                 >
                 Whatsapp
               </a>
--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -42,20 +42,21 @@ const Skills = () => {
 
   return (
     <section id="Skills" className="flex flex-col items-center w-full h-fit p-5">
-      <div className='flex flex-col items-center w-[70%] h-fit '>
-        <div className="flex items-start w-[70%] h-[100px] my-7">
-          <img  src={Skills_png} alt="Skills" className="relative z-0 h-full w-[25%] drop-shadow-xl mx-auto" />        
+      <div className='flex flex-col items-center w-full h-fit '>
+        <div className="flex items-start w-full md:w-[70%] md:h-[100px] md:my-7">
+          <p className="md:hidden pl-6 pr-3 text-2xl font-bolder border-r-[1px] border-[#333]">SKILLS</p>
+          <img  src={Skills_png} alt="Skills" className="hidden md:block relative z-0 h-full w-[25%] drop-shadow-xl mx-auto" />        
         </div>
         <div className='flex items-center justify-center h-fit'>
-          <div className='flex flex-wrap items-center justify-center w-[60%] h-fit gap-3 p-4 '>
-            <p className='flex justify-center w-full py-3 text-4xl text-[#333] font-bolder rounded-md shadow-md'>HardSkills</p>
+          <div className='flex flex-wrap items-center justify-center md:w-[60%] h-fit gap-3 py-6 md:p-4'>
+            <p className='hidden md:flex justify-center w-full py-3 text-4xl text-[#333] font-bolder rounded-md shadow-md'>HardSkills</p>
               {skillsT.map((s)=>{
                 return(
                   <div className='flex flex-col items-center'>
                     <div className='flex items-center justify-center h-[80px] w-[80px] shadow-md rounded-md bg-[#f6f6f6] cursor-pointer'>
                       {s.icon ? s.icon : null}
                     </div>
-                    <p className='text-sm'>{s.name}</p>
+                    <p className='text-sm mt-2'>{s.name}</p>
                   </div>
                 )
               })}
